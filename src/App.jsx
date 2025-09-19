e743908d71fddcb4c712d65a69631ad76aaa3e09
jammandapstudent-design <jammandap.student@ua.edu.ph>
2025-09-19 17:06:57 +0800

    Updated header with John Adrielle Mandap and Footer Container

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,55 @@
 import React from "react";
 import "./App.css";
 
+
+const Header = ({ firstName, lastName }) => (
+  <header className="section top">
+    <div className="top-box"></div>
+    <div className="center">
+      <div className="center-inner-container">
+        <div className="center-inner">{firstName}</div>
+        <div className="center-inner">{lastName}</div>
+      </div>
+    </div>
+    <div className="top-box"></div>
+  </header>
+);
+
+
+const MiddleSection = () => (
+  <main className="section middle">
+    <div className="mid-left"></div>
+    <div className="mid-right"></div>
+    <div className="mid-right"></div>
+  </main>
+);
+
+
+const Footer = ({ items }) => (
+  <footer className="section bottom">
+    {items.map((item, index) => (
+      <div key={index} className={`bottom-box ${item.className || ""}`}>
+        {item.content}
+      </div>
+    ))}
+  </footer>
+);
+
+
 function App() {
+ 
+  const footerItems = [
+    { content: "", className: "purple" },
+    { content: "C-PCIT9", className: "" },
+    { content: "IT3B", className: "" },
+    { content: "", className: "purple" },
+  ];
+
   return (
     <div className="container">
-      {/* Top Section */}
-      <div className="section top">
-        <div className="top-box"></div>
-        <div className="center">
-          <div className="center-inner-container">
-            <div className="center-inner"></div>
-            <div className="center-inner"></div>
-          </div>
-        </div>
-        <div className="top-box"></div>
-      </div>
-
-      {/* Middle Section */}
-      <div className="section middle">
-        <div className="mid-left"></div>
-        <div className="mid-right"></div>
-        <div className="mid-right"></div>
-      </div>
-
-      {/* Bottom Section */}
-      <div className="section bottom">
-        <div className="bottom-box purple"></div>
-        <div className="bottom-box"></div>
-        <div className="bottom-box"></div>
-        <div className="bottom-box purple"></div>
-      </div>
+      <Header firstName="John Adrielle" lastName="Mandap" />
+      <MiddleSection />
+      <Footer items={footerItems} />
     </div>
   );
 }
